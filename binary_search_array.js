44e160aae157eb00a324d6b995c046c8d825122d
George <georgejweiler@gmail.com>
2016-08-24 19:26:13 -0700

    solved binary_search_array

---
+++ binary_search_array.js
@@ -0,0 +1,14 @@
+// Given a sorted array of integers, find the index of a target value using a binary search algorithm.
+
+let binarySeachArray = function(arr, target){
+  var start = 0;
+  var end = arr.length -1;
+  while (start <= end){
+    let pivotIdx = Math.floor((start + end) /2);
+    console.log(arr[pivotIdx])
+    if(arr[pivotIdx] === target){ return pivotIdx }
+    arr[pivotIdx] < target ? start = pivotIdx -1 : end = pivotIdx -1;
+  } 
+  return -1
+}
+console.log(binarySeachArray([ 1, 22, 33, 45, 53, 62 ], 45))
